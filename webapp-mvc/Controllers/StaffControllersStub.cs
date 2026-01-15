2a8937d56ce062b607230a76b851bc13f9510178
insenseemarch <tranthithuytien1003@gmail.com>
2026-01-15 22:34:04 +0700

    fix: remove duplicate BaoTriController from stub file

--- webapp-mvc/Controllers/StaffControllersStub.cs
+++ webapp-mvc/Controllers/StaffControllersStub.cs
@@ -2,21 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace webapp_mvc.Controllers
 {
-    public class BaoTriController : Controller
-    {
-        public IActionResult Index()
-        {
-            var vaiTro = HttpContext.Session.GetString("VaiTro");
-            bool hasAccess = vaiTro?.Equals("Kỹ thuật", StringComparison.OrdinalIgnoreCase) == true ||
-                           vaiTro?.Equals("Quản lý", StringComparison.OrdinalIgnoreCase) == true;
-            if (!hasAccess)
-            {
-                return Unauthorized();
-            }
-            return View();
-        }
-    }
-
     public class QuanLyNhanSuController : Controller
     {
         public IActionResult Index()
